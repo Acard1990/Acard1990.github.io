edc1a9eb547acdf4ab51cbb5dd2d1273aab16c92
Scott Cardinali <ScottCardinali@scotts-mbp.attlocal.net>
2017-09-01 19:47:57 -0400

    jQuery HW Crystal Game

--- assets/game.js
+++ assets/game.js
@@ -1,89 +1,98 @@
+//4 crystals
+//crystals have random number result
+//new number shoud be generated everytime we win or lose
+//when clicking crystal it should add to previous result
+//if not equal we start over
+//if equal then we increment a win counter
 
-var machineChoices = ['audi', 'bmw', 'porsche', 'volkswagen', 'jeep', 'lexus', 'maserati'];
-var validInputs = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-var numWins = 0;
-var numLosses = 0;
-var guessLeft;
-var incorrectGuesses;
-var userGuess;
-var computerWord = '';
-var wordDisplay = '';
-var numWrong = 0;
-
-window.onload = function() {
-
-function setWordDashes() {
-  for (var i = 0; i < computerWord.length; i++)
-    wordDisplay.push('_');
-}
-
-function updateWordDisplay() {
-  for (var j = 0; j < computerWord.length; j++) {
-    if (computerWord[j] === userGuess)
-      wordDisplay[j] = computerWord[j];
-  }
-}
-// Game reset
-function gameReset() {
-  computerWord = machineChoices[Math.floor(Math.random() * machineChoices.length)].toUpperCase();
-  guessLeft = 12;
-  incorrectGuesses = [];
-  userGuess = '';
-  wordDisplay = [];
-  setWordDashes();
-  numWrong = 0;
-}
-
-function gameOver() {
-  numLosses++;
-  setTimeout(function() {
-    alert("YOU LOST\nIt was actually\n" + computerWord.toUpperCase());
-  }, 0);
-  gameReset();
-}
+var random_result;
+var losses = 0;
+var win = 0;
+var previous = 0;
 
 
-gameReset();
-document.querySelector('#numWins').innerHTML = "" + numWins;
-document.querySelector('#numLosses').innerHTML = "" + numLosses;
-document.querySelector('#guessLeft').innerHTML = "" + guessLeft;
-document.querySelector('#wordDisplay').innerHTML = wordDisplay.join(" ");
+var resetAndStart = function() {
+
+  $(".crystals").empty();
+
+  var images = [
+    'http://www.totpi.com/wp-content/uploads/2016/11/0202121-21-1500x840.jpg',
+    'http://www.tiffany.com/shared/images/engagement/uv-dark-mobile.png',
+    'http://channelingerik.com/wp-content/uploads/2015/12/Crystal-Fantasy-Wallpaper-Desktop.jpg',
+    'http://wlpapers.com/images/beautiful-colors-1.jpg'
+  ];
+
+  random_result = Math.floor(Math.random() * 69) + 30;
 
 
-document.onkeyup = function(event) {
+  $("#result").html('Good luck: ' + random_result);
 
-  userGuess = event.key.toUpperCase();
+  for (var i = 0; i < 4; i++) {
 
-  console.log(computerWord);
-  console.log(userGuess);
+    var random = Math.floor(Math.random() * 11) + 1;
 
-  if (validInputs.indexOf(userGuess) === -1) {
-    alert('Invalid Input')
-  } else if ((wordDisplay.indexOf(userGuess) === -1) && (incorrectGuesses.indexOf(userGuess) === -1)) {
-    if (computerWord.indexOf(userGuess) > -1) {
-      updateWordDisplay();
-      if (wordDisplay.indexOf('_') === -1) {
-        numWins++;
-        alert("You nailed it!\n" + computerWord.toUpperCase() + "\nwas the answer");
-        gameReset();
-      }
-    } else {
-      guessLeft--;
-      if (guessLeft > 0) {
-        console.log(incorrectGuesses);
-        incorrectGuesses.push(userGuess);
-        numWrong++;
-        hang();
-      } else {
-        gameOver();
-      }
-    }
+    var crystal = $("<div>");
+
+    crystal.attr({
+      "class": 'crystal transform',
+      "data-random": random
+    });
+
+    crystal.css({
+      "background-image": "url('" + images[i] + "')",
+      "background-size": "cover"
+    });
+
+
+    $(".crystals").append(crystal);
+
   }
 
-  document.querySelector('#numWins').innerHTML = "" + numWins;
-  document.querySelector('#numLosses').innerHTML = "" + numLosses;
-  document.querySelector('#guessLeft').innerHTML = "" + guessLeft;
-  document.querySelector('#incorrectGuesses').innerHTML = incorrectGuesses.join(" ");
-  document.querySelector('#wordDisplay').innerHTML = wordDisplay.join(" ");
-};
+  $("#previous").html("Total Score: " + previous);
+
 }
+
+
+resetAndStart();
+
+
+$(document).on('click', ".crystal", function() {
+
+  $('.transform').toggleClass('transform-active');
+  //still working on the .transform-reset.  Not sure
+  //if I should create a loop that only runs once or
+  //possibly do a conditional statement for the effect.
+
+  var num = parseInt($(this).attr('data-random'));
+
+  previous += num;
+
+  $("#previous").html("Total score: " + previous);
+
+  console.log(previous);
+
+  if (previous > random_result) {
+
+    losses++;
+
+    $("#losses").html("You lost: " + losses);
+
+    previous = 0;
+
+    resetAndStart();
+
+  } else if (previous === random_result) {
+
+    win++;
+    //still working on this congratulatory spin
+    //$('.transform').toggleClass('winning-spin');
+
+    $("#win").html("You win: " + win);
+
+    previous = 0;
+
+    resetAndStart();
+
+  }
+
+});
